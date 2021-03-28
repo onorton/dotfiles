a355d9d35d438cab2169e4fa4a1f720b4df1bfae
Oliver Norton <onorton125@gmail.com>
2021-03-28 16:37:41 +0100

    Add notifications and dwmblocks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,16 @@
-#/bin.sh
+#/bin/sh
+# force main monitor to be primary 
+xrandr --output HDMI-0 --primary
 
 xcompmgr &
 nitrogen --restore &
+dwmblocks 2>&1 >/dev/null &
+
+XNOTIFY_FIFO="$HOME/.cache/xnotify$DISPLAY.fifo"
+export XNOTIFY_FIFO
+rm -f $XNOTIFY_FIFO
+mkfifo $XNOTIFY_FIFO
+xnotify -g -5+20 0<>$XNOTIFY_FIFO &
+echo $XNOTIFY_FIFO
+tiramisu -j | jq --raw-output --unbuffered '.summary + "\t" + .body' > $XNOTIFY_FIFO &
 
